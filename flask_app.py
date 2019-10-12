e462abb0a844c6aa52640db0c60ad2ccd0489c69
iamaperson-web <56465665+iamaperson-web@users.noreply.github.com>
2019-10-13 01:25:13 +1100

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, flash, redirect, request, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy 
+
+
+
+
+
+
+
+
+
+
